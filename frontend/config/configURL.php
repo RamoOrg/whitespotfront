f8771835635983732c2e47b5b8e432a24fefaf64
Janish-1 <janish.pancholi11@gmail.com>
2024-04-16 12:14:07 +0530

    fixed

--- frontend/config/configURL.php
+++ frontend/config/configURL.php
@@ -1,8 +1,8 @@
 <?php
 
-$base_url= 'http://whitespotfront-ab73.onrender.com/';
-$assets_url ="http://whitespotfront-ab73.onrender.com/frontend/frontassets/";
-$upload_url = 'http://whitespotfront-ab73.onrender.com/frontend/frontassets/';
+$base_url= 'https://whitespotfront-ab73.onrender.com/';
+$assets_url ="https://whitespotfront-ab73.onrender.com/frontend/frontassets/";
+$upload_url = 'https://whitespotfront-ab73.onrender.com/frontend/frontassets/';
 
 // define( 'ABSPATH', dirname( __FILE__ ) . '/' );
 ?>
